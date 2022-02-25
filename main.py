048a6f33d0ccf86969f11e6adaa996bde78c0cd6
mateusmota96 <mateus.b.mota@gmail.com>
2022-02-25 16:31:43 -0300

    Commit to update modified files

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 # THIS BOT SEND MESSAGE IN TELEGRAM_BOT WHEN YOUR WEBSITE IS DOWN
 import time
-from dict import error_dict
+from configparser import ConfigParser
 import telebot
 import requests
 import json
 
+parser = ConfigParser()
+parser.read('config.ini')
 bot = telebot.TeleBot("5119858236:AAHemOxB9kyL94jAMVSfRaDA9nG0nKpUB8Y")
 limit = 9999
 client = 'SERRANO'
 
 while True:
-    urlapi = "http://api.esweb.com.br/domain/list?limit=" + str(limit) + "&client=" + client
+    urlapi = "http://sentinel-web.api.esweb.com.br/domain/list?limit=" + str(limit) + "&client=" + client
     req = requests.get(urlapi)
     arrayjson = json.loads(req.content)
     for element in arrayjson:
@@ -21,25 +23,26 @@ while True:
         notify = int(element['notify'])
         identify = element['id']
         send_notify = element['send_notify']
+        print(url, http_code, status, error, notify, identify, send_notify)
 
         if http_code != 200 and http_code != 403 and http_code != 301 and http_code != 302 and \
                 (error == 1 and notify == 1 and send_notify == 0):
             # STATUS
             status = "'ERROR'"
-            dict = str(http_code)
+            error_dict = parser.get('http_error', str(http_code))
             strstatus = status.replace("'", "")
             message_http = "❌ [<b>" + strstatus + "</b>]\n"
 
             # MESSAGE
             server = "<b>Server:</b> " + url + "\n<b>Status Code:</b> [" + str(http_code) + "] - "
-            message = message_http + server + error_dict[dict]
+            message = message_http + server + error_dict
             data = {"id": str(identify)}
             try:
                 bot.send_message('-776182901', message, parse_mode='html')
                 # UNSET NOTIFY -> notify = 0
-                requests.post('http://api.esweb.com.br/notify/post', data)
+                requests.post('http://sentinel-web.api.esweb.com.br/notify/post', data)
                 # SET SENDNOTIFY -> send_notify = 1
-                requests.post('http://api.esweb.com.br/delnotify/postsend', data)
+                requests.post('http://sentinel-web.api.esweb.com.br/delnotify/postsend', data)
             except TimeoutError:
                 print("Timeout ERROR")
             except:
@@ -48,22 +51,19 @@ while True:
         elif (http_code == 200 or http_code == 301 or http_code == 302) and send_notify == 1:
             # STATUS
             status = "'RE-UP'"
-            dict = str(http_code)
+            error_dict = parser.get('http_error', str(http_code))
             strstatus = status.replace("'", "")
             message_http = "✅ [<b>" + strstatus + "</b>]\n"
 
             # MESSAGE
             server = "<b>Server:</b> " + url + "\n<b>Status Code:</b> [" + str(http_code) + "] - "
-            message = message_http + server + error_dict[dict] + "\n<i>Server is Up Again!</i>"
+            message = message_http + server + error_dict + "\n<i>Server is Up Again!</i>"
             data = {"id": str(identify)}
-            try:
-                # UNSET SENDNOTIFY -> send_notify = 0
-                requests.post('http://api.esweb.com.br/delnotify/delsend', data)
-                bot.send_message('-776182901', message, parse_mode='html')
 
-            except TimeoutError:
-                print("Timeout ERROR")
-            except:
-                print("UNKNOWN ERROR")
+            # UNSET SENDNOTIFY -> send_notify = 0
+            requests.post('http://sentinel-web.api.esweb.com.br/delnotify/delsend', data=data)
+            print("teste")
+            bot.send_message('-776182901', message, parse_mode='html')q
+
 
     time.sleep(10)
--- web-monitoring.py
+++ web-monitoring.py
@@ -1,27 +1,46 @@
 import requests
 import json
 from configparser import ConfigParser
+from requests import ReadTimeout
 
 parser = ConfigParser()
 parser.read('config.ini')
 
 urls = [
     'http://www.sitedomotao.com.br',
-    'http://www.localhost/'
+    'http://localhost/'
 ]
 
 while True:
     for url in urls:
-        req = requests.head(url)
-        http_code = int(req.status_code)
-        status = parser.get('http_error', str(http_code))
-        str_req_api = 'http://sentinel-webtest.api.mateus.com.br/domain/list?limit=999&url=' + str(url)
-        req_api = requests.get(str_req_api)
-        json_content = req_api.content
-        dict = json.loads(json_content)
-        error = dict[0]['error']
-        http_ok = [200, 301, 302, 403]
-        if http_code not in http_ok and error == 0:
+        try:
+            req = requests.head(url)
+            http_code = int(req.status_code)
+            status = parser.get('http_error', str(http_code))
+            str_req_api = 'http://sentinel-web.api.esweb.com.br/domain/list?limit=999&url=' + url
+            req_api = requests.get(str_req_api)
+            json_content = req_api.content
+            dict = json.loads(json_content)
+            error = dict[0]['error']
+            print(http_code, error)
+            http_ok = [200, 301, 302, 403]
+            if http_code not in http_ok and error == 0:
+                data = {"url": str(url), "http_code": str(http_code), "status": status}
+                # POST IN API, TO CHANGE ERROR = 1 AND NOTIFY = 1
+                post_req = requests.post('http://sentinel-web.api.esweb.com.br/error/set', data)
+            elif http_code in http_ok and error == 1:
+                print("entrei aqui")
+                data = {"url": str(url), "http_code": str(http_code), "status": status}
+                # POST IN API, TO CHANGE ERROR = 1 AND NOTIFY = 1
+                post_req = requests.post('http://sentinel-web.api.esweb.com.br/error/unset', data)
+        except ReadTimeout:
+            http_code = 998
+            status = parser.get('http_error', str(http_code))
             data = {"url": str(url), "http_code": str(http_code), "status": status}
-            # POST IN API, TO CHANGE ERROR = 1 AND NOTIFY = 1
-            post_req = requests.post('http://sentinel-webtest.api.mateus.com.br/error/set', data)
+            post_req = requests.post('http://sentinel-web.api.esweb.com.br/error/set', data)
+        except:
+            http_code = 999
+            status = parser.get('http_error', str(http_code))
+            data = {"url": str(url), "http_code": str(http_code), "status": status}
+            post_req = requests.post('http://sentinel-web.api.esweb.com.br/error/set', data)
+
